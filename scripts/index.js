a4f8a6ce03514033d6b84b6874cab58775e14a01
ShashikaUdara <techdudelab@gmail.com>
2022-02-13 21:34:43 +0530

    billing list

--- scripts/index.js
+++ scripts/index.js
@@ -8,12 +8,22 @@
 //     $('p').last().after($node);
 // }
 
-function addNewTextField() 
+// function addNewTextField() 
+// {
+//     // document.getElementById("demo").innerHTML = "Paragraph changed.";
+//     var $fieldCount = 0;
+//     $fieldCount++;
+//     var $node = '<p><label for="text'+$fieldCount+'">Text '+$fieldCount+': </label><input type="text" name="text'+$fieldCount+'" id="text'+$fieldCount+'"/><span class="removeField">Remove</span></p>';
+//     document.getElementById("demo").innerHTML = $node;
+//     $('p').last().after($node);
+// }
+let template_field = "";
+let fieldCount = 0;
+
+function addNewTextField()
 {
-    // document.getElementById("demo").innerHTML = "Paragraph changed.";
-    var $fieldCount = 0;
-    $fieldCount++;
-    var $node = '<p><label for="text'+$fieldCount+'">Text '+$fieldCount+': </label><input type="text" name="text'+$fieldCount+'" id="text'+$fieldCount+'"/><span class="removeField">Remove</span></p>';
-    document.getElementById("demo").innerHTML = $node;
-    $('p').last().after($node);
+    // creating the template
+    fieldCount++;
+    template_field += `<li id="dynamic-list"><label class="label${fieldCount}">Item#${fieldCount}:<input id="item-text" class="field-no-item-${fieldCount}" type="text"><input id="item-amount" class="field-no-amount-${fieldCount}" type="text"></label></li>`;
+    document.getElementById("bill-list").innerHTML = "<ul>" + template_field + "</ul>"
 }
